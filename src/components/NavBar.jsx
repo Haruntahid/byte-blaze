3778e422c6cd092bbdf57674b8f7de58b2076c34
haruntahid <tahidtaha997@gmail.com>
2024-03-21 11:22:36 +0600

    initialize the project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+function NavBar() {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">
+            Byte<span>Blaze</span>
+          </a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Blogs</a>
+            </li>
+            <li>
+              <a>Bookmarks</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,9 @@
+function Blogs() {
+  return (
+    <>
+      <h1>blogs</h1>
+    </>
+  );
+}
+
+export default Blogs;
